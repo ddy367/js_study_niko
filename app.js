48a127581bc1dadac0bf5bc0ed4f57c6c4d658bb
KDY <merry4739@naver.com>
2022-04-05 18:13:27 +0900

    04.05 / 2.4

--- app.js
+++ app.js
@@ -10,3 +10,27 @@ console.log("hello " + myName);
 myName = "nicolas";
 
 console.log("your new name is  " + myName);
+
+/////////////////////////////
+
+const amIFat = null;
+let something;
+console.log(something, amIFat);
+// undefine 값이 없다 정의되지 않았다   null 비어있다
+
+/////////////////////////////
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+////////////////////////////
+
+const playName = "nico";
+cosnt playerPoints = 12121212
